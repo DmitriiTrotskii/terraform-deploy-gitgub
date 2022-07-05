a207ee7c01f59aad7d44866671c75265406b62f6
Дмитрий Троцкий <dmitrii.trotskii@gmail.com>
2022-07-05 10:42:53 +0700

    change description comment

--- example/backend.tf
+++ example/backend.tf
@@ -4,13 +4,13 @@ terraform {
   backend "s3" {
     endpoint                    = "storage.yandexcloud.net"
 
-    # Make S3 bucket
+    # Create S3 bucket
     # https://cloud.yandex.ru/docs/storage/operations/buckets/create
     bucket                      = "< Bucket name >"
     region                      = "< Backet region >"
     key                         = "< path/name.tfstate >"
 
-    # Make service account and access key
+    # Create service account and access key
     # https://cloud.yandex.ru/docs/iam/operations/sa/create
     # https://cloud.yandex.ru/docs/iam/operations/sa/create-access-key
     access_key                  = "< Service account kye id >"
